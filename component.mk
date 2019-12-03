48030e0df44f7ae372743414f200212d6c0c445c
Franz Auernigg <f.auernigg@commend.com>
2019-12-03 10:03:31 +0100

    update component.mk

--- component.mk
+++ component.mk
@@ -2,3 +2,7 @@ COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENT_PRIV_INCLUDEDIRS := include
 COMPONENT_SRCDIRS := src
 
+
+-include components/baresip/component.mk
+-include components/re/component.mk
+-include components/rem/component.mk
